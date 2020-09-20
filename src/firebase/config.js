29bdd8e0c34356548f9611c27eed259d3ab8c2b6
Jayesh Singh <jayeshsingh001@gmail.com>
2020-09-20 23:48:33 +0530

    final commit

---
+++ src/firebase/config.js
@@ -0,0 +1,8 @@
+export const config = {
+  apiKey: "AIzaSyCylVdNJV8lQQx_R2bHCDdX-mq_CKXCWgE",
+  authDomain: "confusionserver-jayesh.firebaseapp.com",
+  databaseURL: "https://confusionserver-jayesh.firebaseio.com",
+  projectId: "confusionserver-jayesh",
+  storageBucket: "confusionserver-jayesh.appspot.com",
+  messagingSenderId: "400460287319",
+};
